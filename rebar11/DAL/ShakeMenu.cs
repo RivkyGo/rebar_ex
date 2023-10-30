9bda276ff83d6b730a0cd96603613ee37aced486
Rivka Goldberg <rg542400@gmail.com>
2023-10-30 18:56:44 +0200

    add ShakeMenu.cs

---
+++ rebar11/DAL/ShakeMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ShakeMenu
+    {
+        public string ShakeName { get; set; }
+        public string ShakeDescription { get; set; }
+        public int ShakeSizeL { get; set; }
+        public int ShakeSizeM { get; set; }
+        public int ShakeSizeS { get; set; }
+        public Guid ShakeID { get; set; }
+
+        public ShakeMenu(string shakeName, string shakeDescription, int shakeSizeL, int shakeSizeM, int shakeSizeS)
+        {
+            ShakeName = shakeName;
+            ShakeDescription = shakeDescription;
+            ShakeSizeL = shakeSizeL;
+            ShakeSizeM = shakeSizeM;
+            ShakeSizeS = shakeSizeS;
+            ShakeID = Guid.NewGuid();
+        }
+    }
+}
