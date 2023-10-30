e39db293d4cdf751f9f287c8e254485f72ed5207
Rivka Goldberg <rg542400@gmail.com>
2023-10-30 19:40:39 +0200

    add classes  Menu.cs

---
+++ rebar11/rebar11/Models/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DAL
+{
+    public class Menu
+    {
+        public List<ShakeMenu> Shakes { get; set; }
+
+        public Menu()
+        {
+            Shakes = new List<ShakeMenu>();
+        }
+
+        public List<ShakeMenu> MenuShow()
+        {
+            return Shakes;
+            //הצגת הרשימה של השיקים הקיימים בתפריט
+        }
+
+        public void AddShake(string shakeName, string shakeDescription, int shakeSizeL, int shakeSizeM, int shakeSizeS)
+        {
+            if (Shakes == null)
+            {
+                Shakes = new List<ShakeMenu>();
+            }
+            ShakeMenu shake = new ShakeMenu(shakeName, shakeDescription, shakeSizeL, shakeSizeM, shakeSizeS);
+            Shakes.Add(shake);
+        }
+    }
+}
